1a9c300a1d6d44f1971ab820eb1e5aceb22782ad
Stubeans <stuart.smith2@marist.edu>
2023-03-14 19:54:32 -0400

    ghetto CST implementation

--- source/Parser.java
+++ source/Parser.java
@@ -9,10 +9,12 @@ public class Parser {
     ArrayList<Token> thisTokenStream;
     private int prgCounter = 0;
     private boolean isntError;
-    ArrayList<String> CST;
-    ArrayList<Integer> CSTdepth;
+    ArrayList<String> CST = new ArrayList<>();
+    ArrayList<Integer> CSTdepth = new ArrayList<>();
+    int depth;
 
     public void main(ArrayList<Token> tokenStream) {
+        depth = 0;
         prgCounter++;
         isntError = true;
         thisTokenStream = tokenStream;
@@ -37,6 +39,9 @@ public class Parser {
 
     private void parsePrg() {
         if(isntError) {
+            CST.add("<Program>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseProgram()");
             parseBlock();
             match("EOP");
@@ -45,15 +50,22 @@ public class Parser {
 
     private void parseBlock() {
         if(isntError) {
+            CST.add("<Block>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseBlock()");
             match("OPEN_BLOCK");
             parseStmtList();
             match("CLOSE_BLOCK");
+            depth--;
         }
     }
 
     private void parseStmtList() {
         if(isntError) {
+            CST.add("<Statement List>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseStatementList()");
             if(currentToken.equals("PRINT_STMT") | currentToken.equals("ID") | currentToken.equals("TYPE_INT") | currentToken.equals("TYPE_STR") | 
             currentToken.equals("TYPE_BOOL") | currentToken.equals("WHILE_STMT") | currentToken.equals("IF_STMT") | currentToken.equals("OPEN_BLOCK")) {
@@ -62,11 +74,15 @@ public class Parser {
             } else {
                 //do nothing
             }
+            depth--;
         }
     }
 
     private void parseStmt() {
         if(isntError) {
+            CST.add("<Statement>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseStatement()");
             if(currentToken.equals("PRINT_STMT")) {
                 parsePrintStmt();
@@ -88,56 +104,80 @@ public class Parser {
                 System.out.println();
                 isntError = false;
             }
+            depth--;
         }
     }
 
     private void parsePrintStmt() {
         if(isntError) {
+            CST.add("<Print Statement>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parsePrintStatement()");
             match("PRINT_STMT");
             match("OPEN_PAREN");
             parseExpr();
             match("CLOSE_PAREN");
+            depth--;
         }
     }
 
     private void parseAssignStmt() {
         if(isntError) {
+            CST.add("<Assign Statement>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseAssignmentStatement()");
             parseId();
             match("ASSIGN_OP");
             parseExpr();
+            depth--;
         }
     }
 
     private void parseVarDecl() {
         if(isntError) {
+            CST.add("<Variable Declaration>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseVariableDeclaration()");
             parseType();
             parseId();
+            depth--;
         }
     }
 
     private void parseWhileStmt() {
         if(isntError) {
+            CST.add("<While Statement>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseWhileStatement()");
             match("WHILE_STMT");
             parseBoolExpr();
             parseBlock();
+            depth--;
         }
     }
 
     private void parseIfStmt() {
         if(isntError) {
+            CST.add("<If Statement>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseIfStatement()");
             match("IF_STMT");
             parseBoolExpr();
             parseBlock();
+            depth--;
         }
     }
     
     private void parseExpr() {
         if(isntError) {
+            CST.add("<Expression>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseExpression()");
             if(currentToken.equals("DIGIT")) {
                 parseIntExpr();
@@ -155,31 +195,43 @@ public class Parser {
                 System.out.println();
                 isntError = false;
             }
+            depth--;
         }
     }
 
     private void parseIntExpr() {
         if(isntError) {
+            CST.add("<Integer Expression>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseIntegerExpression()");
             parseDigit();
             if(currentToken == "INTOP") {
                 parseIntop();
                 parseExpr();
             }
+            depth--;
         }
     }
 
     private void parseStringExpr() {
         if(isntError) {
+            CST.add("<String Expression>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseStringExpression()");
             match("OPEN_STR");
             parseCharList();
             match("CLOSE_STR");
+            depth--;
         }
     }
 
     private void parseBoolExpr() {
         if(isntError) {
+            CST.add("<Boolean Expression>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseBoolExpression()");
             if(currentToken.equals("OPEN_PAREN")) {
                 match("OPEN_PAREN");
@@ -197,18 +249,26 @@ public class Parser {
                 System.out.println();
                 isntError = false;
             }
+            depth--;
         }
     }
 
     private void parseId() {
         if(isntError) {
+            CST.add("<Id>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseId()");
             match("ID");
+            depth--;
         }
     }
 
     private void parseCharList() {
         if(isntError) {
+            CST.add("<Char List>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseCharList()");
             if(currentToken.length() >= 4) {
                 if(currentToken.substring(0, 4).equals("CHAR")) {
@@ -223,11 +283,15 @@ public class Parser {
             } else {
                 //nothing
             }
+            depth--;
         }
     }
 
     private void parseType() {
         if(isntError) {
+            CST.add("<Type>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseType()");
             if(currentToken.equals("TYPE_INT")) {
                 match("TYPE_INT");
@@ -243,48 +307,73 @@ public class Parser {
                 System.out.println();
                 isntError = false;
             }
+            depth--;
         }
     }
 
     private void parseChar() {
         if(isntError) {
+            CST.add("<Char>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseChar()");
             match("CHAR");
+            depth--;
         }
     }
 
     private void parseSpace() {
         if(isntError) {
+            CST.add("<Space>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseSpace()");
             match("_CHAR_SPACE");
+            depth--;
         }
     }
 
     private void parseDigit() {
         if(isntError) {
+            CST.add("<Digit>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseDigit()");
             match("DIGIT");
+            depth--;
         }
     }
 
     private void parseBoolop() {
         if(isntError) {
+            CST.add("<Boolean Operation>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseBoolOperation()");
             match("BOOL_OP");
+            depth--;
         }
     }
 
     private void parseBoolval() {
         if(isntError) {
+            CST.add("<Boolean Value>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseBoolValue()");
             match("BOOL_VAL");
+            depth--;
         }
     }
 
     private void parseIntop() {
         if(isntError) {
+            CST.add("<Integer Operation>");
+            CSTdepth.add(depth);
+            depth++;
             debug("parseIntegerOperation()");
             match("INTOP");
+            depth--;
         }
     }
 
@@ -296,9 +385,12 @@ public class Parser {
                 }
             }
             if(currentToken.equals(expectedToken)) {
+                CST.add("[" + thisTokenStream.get(currentTokenPos).val + "]");
+                CSTdepth.add(depth);
                 if(expectedToken.equals("EOP")) {
                     debug("Parse completed successfully");
                     System.out.println();
+                    displayCST();
                 } else {
                     currentTokenPos++;
                 }
@@ -316,5 +408,21 @@ public class Parser {
 
     private void debug(String message) {
         System.out.println("PARSER: " + message);
+    }
+
+    private void displayCST() {
+        System.out.println("CST for program " + prgCounter + "...");
+        String leading = "";
+        for(int i = 0; i < CST.size(); i++) {
+            for(int j = 0; j < CSTdepth.get(i); j++) {
+                leading = leading + "-";
+            }
+            System.out.println(leading + CST.get(i));
+            leading = "";
+        }
+        System.out.println();
+        CST.clear();
+        CSTdepth.clear();
+        depth = 0;
     }
 }
