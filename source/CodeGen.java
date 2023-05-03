b7e51b8baf62d95a1dd7c82dcff49c200e69b0dd
Stubeans <stuart.smith2@marist.edu>
2023-05-02 22:45:06 -0400

    Added basic integer support for codeGen

--- source/CodeGen.java
+++ source/CodeGen.java
@@ -20,6 +20,8 @@ public class CodeGen {
     ArrayList<Integer> scopeList = new ArrayList<>();
     int lastDepth = -500;
 
+    int tempNum = 0;
+
     public void main(ArrayList<String> iAST, ArrayList<Integer> iASTdepth, ArrayList<Symbol> isymbolTable) {
         System.out.println("Starting Code Gen...");
         System.out.println();
@@ -30,18 +32,46 @@ public class CodeGen {
         codeGeneration();
         stackGen();
         heapGen();
-        if(opCodes.size() > 256) {
+        if(opCodes.size() > 256 || codePointer > 256) {
             //ERROR
         } else {
-            //done
+            printCode();
         }
     }   
 
     private void codeGeneration() {
         for(int i = 0 ; i < AST.size(); i++) {
             if(AST.get(i).equals("<Variable Declaration>")) {
-                
+                //System.out.println("VarDecl");
+                i++;
+                i++;
+                stack.add(new tempVar("T" + Integer.toString(tempNum) + "XX", AST.get(i).substring(1, 2), Integer.toString(tempNum), scope, findScopeLetter(scope)));
+                addToCode("A9");
+                addToCode("00");
+                addToCode("8D");
+                addToCode("T" + Integer.toString(tempNum));
+                addToCode("XX");
+                tempNum++;
             } else if(AST.get(i).equals("<Assign Statement>")) {
+                i++;
+                tempVar tempVariable = isInTempTable(stack, AST.get(i).substring(1, 2), scope, findScopeLetter(scope));
+                i++;
+                addToCode("A9");
+                //addToCode("00");
+                boolean isntDone = true;
+                int wholeNum = 0;
+                while(isntDone) {
+                    wholeNum = wholeNum + Integer.valueOf(AST.get(i).substring(1, 2));
+                    if(AST.get(i + 1).length() > 3) {
+                        isntDone = false;
+                    } else {
+                        i++;
+                    }
+                }
+                addToCode(num2hex(wholeNum));
+                addToCode("8D");
+                addToCode(tempVariable.temp.substring(0, 2));
+                addToCode("XX");
 
             } else if(AST.get(i).equals("<If Statement>")) {
 
@@ -50,10 +80,12 @@ public class CodeGen {
             } else if(AST.get(i).equals("<Print Statement>")) {
 
             } else if(AST.get(i).equals("<Block>")) {
+                //System.out.println("Block");
                 scope++;
                 scopeList.add(scope);
                 lastDepth = ASTdepth.get(i);
             } else if(AST.get(i).equals("<endBlock>")) {
+                //System.out.println("endBlock");
                 scope--;
             }
 
@@ -61,11 +93,28 @@ public class CodeGen {
     }
 
     private void stackGen() {
-
+        //System.out.println("stackGen");
+        addToCode("00");
+        for(int i = 0; i < stack.size(); i++) {
+            for(int j = 0; j < opCodes.size(); j++) {
+                if(opCodes.get(j).equals("T" + Integer.toString(i))) {
+                    opCodes.set(j, num2hex(codePointer));
+                    j++;
+                    opCodes.set(j, "00");
+                }
+            }
+            codePointer++;
+        }
+        //System.out.println("end stackGen");
     }
 
     private void heapGen() {
 
+    }
+
+    private void addToCode(String code) {
+        opCodes.add(codePointer, code);
+        codePointer++;
     }
 
     private String findScopeLetter(int inputScope) {
@@ -81,6 +130,17 @@ public class CodeGen {
         return scopeLetter;
     }
 
+    private String num2hex(int num) {
+        String hexx = Integer.toHexString(num);
+        hexx = hexx.toUpperCase();
+        if(hexx.length() < 2) {
+            hexx = "0" + hexx;
+        } else if(hexx.length() > 2) {
+            hexx = hexx.substring(0, 2);
+        }
+        return hexx;
+    }
+
     private Symbol isInTableS(ArrayList<Symbol> checkST, String checkName, int iscope, String iScopeLetter) {
         for(int i = iscope; i > -1; i--) {
             for(int j = 0; j < checkST.size(); j++) {
@@ -92,6 +152,35 @@ public class CodeGen {
             }
         }
         return null;
+    }
+
+    private tempVar isInTempTable(ArrayList<tempVar> checkST, String checkName, int iscope, String iScopeLetter) {
+        for(int i = iscope; i > -1; i--) {
+            for(int j = 0; j < checkST.size(); j++) {
+                if(checkST.get(j).var.equals(checkName) && checkST.get(j).scope == i && checkST.get(j).scopeLetter.equals(iScopeLetter) && i == iscope) {
+                    return checkST.get(j);
+                } else if(checkST.get(j).var.equals(checkName) && checkST.get(j).scope == i && i != iscope) {
+                    return checkST.get(j);
+                }
+            }
+        }
+        return null;
+    }
+
+    private void printCode() {
+        boolean isntEnd = true;
+        int pointer = 0;
+        // while(isntEnd) {
+        //     if(opCodes.get(pointer).equals("00")) {
+        //         isntEnd = false;
+        //         break;
+        //     }
+        //     System.out.print(opCodes.get(pointer) + " ");
+        //     pointer++;
+        // }
+        for(int i = 0; i < opCodes.size(); i++) {
+            System.out.print(opCodes.get(i) + " ");
+        }
     }
     
 }
